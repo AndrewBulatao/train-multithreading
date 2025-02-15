6f2ee1e3981915fea4bacc033cef216318aadb90
Andrew <andrewbulatao77@gmail.com>
2025-02-15 16:36:58 -0500

    Uploading given code and changing the package name they came from

---
+++ given_code/DisplayCharSequence.java
@@ -0,0 +1,33 @@
+package given_code;
+
+//Example of creating threads by extending TimerTask class
+//Displays characters in separate threads
+import java.util.*;
+
+public class DisplayCharSequence extends TimerTask {
+	private char displayChar;
+	Timer timer;
+	
+	//constructor for character displayer
+	public DisplayCharSequence(char c){
+	   displayChar = c;
+	   timer = new Timer();
+	   timer.schedule(this, 0);
+	}
+	
+	//display the occurrences of the character
+	public void run() {
+	    for (int i = 0; i < 5; ++i) {
+	        System.out.print(displayChar + " ");
+	    }
+	    timer.cancel();
+	}
+	
+	//main
+	public static void main (String[] args) {
+	   new DisplayCharSequence('M');
+	   new DisplayCharSequence('A');
+	   new DisplayCharSequence('R');
+	   new DisplayCharSequence('K');
+	}
+}
---
+++ given_code/RunnableTester.java
@@ -0,0 +1,36 @@
+package given_code;
+
+//Multiple threads printing at different intervals.
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+
+public class RunnableTester 
+{
+	public static void main( String[] args )
+	{
+	   // create and name each runnable
+	   PrintTask task1 = new PrintTask( "Thread-1" );
+	   PrintTask task2 = new PrintTask( "Thread-2" );
+	   PrintTask task3 = new PrintTask( "Thread-3" );
+	     
+	   System.out.println( "* * Starting Threads \n" );
+	
+	   // create ExecutorService to manage threads
+	   ExecutorService threadExecutor = Executors.newCachedThreadPool();
+	
+	   // Starting threads
+	   System.out.println( "* * All Threads Started . . .\n" );
+	   // start threads and place in runnable state
+	   threadExecutor.execute( task1 ); // start task1
+	   threadExecutor.execute( task2 ); // start task2
+	   threadExecutor.execute( task3 ); // start task3
+	   
+	   // Shutdown the ExecutorService object
+	   threadExecutor.shutdown(); 
+	   while (!threadExecutor.isTerminated()) 
+	   {
+	 	  //wait for all threads to terminate
+	   }     
+	   System.out.println( "\n* * All Threads Terminated. Main Ends.\n" );
+	} // end main
+} // end class RunnableTester
