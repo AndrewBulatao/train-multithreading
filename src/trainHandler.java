1d5fee3e3223fcd6a798e3cd7686013e6cb31e6c
Andrew <andrewbulatao77@gmail.com>
2025-02-17 15:43:22 -0500

    Created a trainHandler file with comments about how to go about the code

---
+++ src/trainHandler.java
@@ -0,0 +1,31 @@
+package src;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class trainHandler{
+    private ArrayList<Train> trainList = new ArrayList<>();
+
+    public boolean pathChecker(int inbound, int outbound){
+
+
+        return true;
+    }
+    // Inserting new train into the list (this wil be using theYardFile.csv)
+    public void insertTrain(String s){
+        // Parse the string
+
+        // Make the string into int. 
+        // Make a boolean that is a checker for hold or perm hold
+
+
+        // Before creating the object, check the path of train
+
+
+        // Create the object
+
+    }
+
+
+
+}
