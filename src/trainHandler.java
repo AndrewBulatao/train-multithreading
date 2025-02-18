0e88533c784f4efc1a889e9cd051f315d71ecdac
Andrew <andrewbulatao77@gmail.com>
2025-02-18 13:39:14 -0500

    Implemented file reader and added logic to handle train data

--- src/trainHandler.java
+++ src/trainHandler.java
@@ -1,9 +1,13 @@
 package src;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
+
 public class trainHandler{
+
+    private HashMap<Integer, List<Integer>> trainMap = new HashMap<>();
 
     public boolean trainChecker(int inbound, int outbound){
         // TODO: CHECK BY USING THE HASH FUNCTION
@@ -11,28 +15,50 @@ public class trainHandler{
         return true;
     }
     // inserting function
-    public void insertTrain(String fleetString, String yardString){
-        // Parse the string
-        String[] fleet = fleetString.split(",");
-        String[] yard = yardString.split(",");
-
+    public void insertTrain(String fleetString, List<String> yardData){
         // Make the string into int. 
-        // FLEET
+        String[] fleet = fleetString.split(",");
 
+        int trainNum = Integer.parseInt(fleet[0]);
+        int inboundTrack = Integer.parseInt(fleet[1]);
+        int outboundTrack = Integer.parseInt(fleet[2]);
+        boolean matchFound = false;
+        
+        // Init switches arrayList
+        List<Integer> switches = null;
+        // Search for the match
+        for (String curYard : yardData){
+            String[] yard = curYard.split("");
+            int yardInbound = Integer.parseInt(yard[0]);
+            int yardOutbound = Integer.parseInt(yard[4]);
 
-        // YARD
+            if (inboundTrack == yardInbound && outboundTrack == yardOutbound){
+                // Insert the switches in the yardFile into a hashmap with the train number
+                switches = new ArrayList<>();
+                switches.add(Integer.valueOf(yard[1]));
+                switches.add(Integer.valueOf(yard[2]));
+                switches.add(Integer.valueOf(yard[3]));
+                matchFound = true;
+                
+                break; // Stop looking
+            }
+        }
+        
+        // We found no match
+        if (!matchFound){
+            switches.add(0);
+            switches.add(0);
+            switches.add(0);
 
-        // Find where the fleet file inbound & outbound = yard are the same. Create a hash with that
+        }
+        trainMap.put(trainNum, switches);
 
-        // If that train number is not found in the yardfile, set switches == 0.
-        // Can also make a checker to see if the 
-        // Before creating the object, check the path of train
+    }// insertTrain end
 
-
-        // Create the object
-
+    // Prints out hashmap
+    public HashMap<Integer, List<Integer>> getTrainMap() {
+        return trainMap;
     }
-
 
 
 }
